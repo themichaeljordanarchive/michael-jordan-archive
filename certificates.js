66377fefcfebb8285b98ba7e0a4d5976a9f1e669
themichaeljordanarchive <themichaeljordanarchive@gmail.com>
2026-06-21 20:44:33 -0400

    up

--- certificates.js
+++ certificates.js
@@ -6,7 +6,7 @@ const certificates = {
     documentId: "MJA-8687-AJ2-SNEAKERS",
     image: "assets/certificates/MJA-8-2024-PM-001.jpg",
   },
-  "MJA-4-2026-PM-002": {
+  "MJA-4-2026-C-002": {
     status: "Verified Archive Record",
     title: "1985–86 Chicago Bulls Michael Jordan Road Jersey",
     issueDate: "April 2026",
