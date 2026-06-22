d00eccdd116a86d1eff7e12611a947a47b24b5c8
themichaeljordanarchive <themichaeljordanarchive@gmail.com>
2026-06-21 21:30:14 -0400

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -1,17 +1,17 @@
 const certificates = {
-  "MJA-8-2024-PM-001": {
+  "MJA-8-2024-C-001": {
     status: "Verified Archive Record",
     title: "1986-87 Michael Jordan Game Used Sneakers",
     issueDate: "August 2024",
-    documentId: "MJA-8687-AJ2-SNEAKERS",
-    image: "assets/certificates/MJA-8-2024-PM-001.jpg",
+    documentId: "MJA-8-2026-PM-001",
+    image: "assets/certificates/MJA-8-2024-C-001.jpg",
   },
   "MJA-4-2026-C-002": {
     status: "Verified Archive Record",
     title: "1985–86 Chicago Bulls Michael Jordan Road Jersey",
     issueDate: "April 2026",
     documentId: "MJA-4-2026-PM-002",
-    image: "assets/certificates/MJA-4-2026-PM-002.jpg",
+    image: "assets/certificates/MJA-4-2026-C-002.jpg",
   },
 };
 
